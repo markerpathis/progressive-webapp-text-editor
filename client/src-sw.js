855fb99da278ea921c70c28d61f2291b0b8e45b7
Parker Mathis <parkerjmathis@gmail.com>
2023-02-22 16:57:19 -0800

    Updated comments

--- client/src-sw.js
+++ client/src-sw.js
@@ -26,7 +26,7 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === "navigate", pageCache);
 
-// TODO: Implement asset caching
+// Sets up asset cache
 registerRoute(
   ({ request }) => ["style", "script", "worker"].includes(request.destination),
   new CacheFirst({
